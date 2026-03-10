7c652664ed59f4c4920ee4e42759b6703b9dd89a
Daniel Essel <dessel015@gmail.com>
2026-03-10 10:36:14 +0000

    updated operator layout

--- app/operator/register/layout.tsx
+++ app/operator/register/layout.tsx
@@ -3,6 +3,8 @@
 import { AuthGuard } from "@/app/components/auth-guard";
 import { ReactNode } from "react";
 
+export const dynamic = "force-dynamic"; // ✅ add this
+
 export default function OperatorLayout({ children }: { children: ReactNode }) {
   return (
     <AuthGuard allowedRoles={["Operator"]} unauthorizedRedirectTo="/dashboard">
